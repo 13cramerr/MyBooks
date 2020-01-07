555455371f83611782e4bafc49b363b19b4c20ff
13cramerr <13cramerr@gmail.com>
2020-01-06 19:27:06 -0600

    Add navigation to newbookpage

--- MyBooks/MyBooks/NewBookPage.xaml.cs
+++ MyBooks/MyBooks/NewBookPage.xaml.cs
@@ -16,5 +16,10 @@ namespace MyBooks
 		{
 			InitializeComponent ();
 		}
-	}
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Success", "We have handled the click event", "Great!");
+        }
+    }
 }
